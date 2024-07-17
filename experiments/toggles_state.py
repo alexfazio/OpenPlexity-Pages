8eee647e7595c2cfaaad85b5adf83ab8132a42f4
Alex Fazio <alessandro.fazio@me.com>
2024-07-17 17:54:16 +0200

    chore(toggle_state): clean up unnecessary functions from toggle_state.py

--- experiments/toggles_state.py
+++ experiments/toggles_state.py
@@ -2,15 +2,3 @@
 
 # Dictionary to store toggle states
 toggle_states = {}
-
-def update_toggle(key, value):
-    """Update the state of a toggle."""
-    toggle_states[key] = value
-
-def get_toggle(key):
-    """Get the state of a toggle."""
-    return toggle_states.get(key, False)  # Default to False if key doesn't exist
-
-def get_all_toggles():
-    """Get all toggle states."""
-    return toggle_states
