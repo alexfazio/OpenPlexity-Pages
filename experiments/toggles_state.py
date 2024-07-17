61c855f1a66c9a447cd106a2c11024d8c321b19a
Alex Fazio <alessandro.fazio@me.com>
2024-07-17 18:06:53 +0200

    feat(toggle): add necessary toggles to the dictionary of toggle states

--- experiments/toggles_state.py
+++ experiments/toggles_state.py
@@ -1,4 +1,13 @@
 # toggles_state.py
 
 # Dictionary to store toggle states
-toggle_states = {}
+toggle_states = {
+    "checkbox_main": False,
+    "checkbox_secondary": False,
+    "checkbox_tertiary": False,
+    "example_tone": False,
+    "keywords": False,
+    "role": False,
+    "style": False,
+    "tone": False
+}
